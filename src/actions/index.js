ae707d77aad6b7824e47f1c3276f4f366d95b710
Zachary <zach.rickards@gmail.com>
2017-03-21 19:01:02 -0400

    Adding ui and logic for creating reminders and deleting reminders

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constants';
+
+export const addReminder = (text, dueDate) => {
+	const action = {
+		type: ADD_REMINDER,
+		text,
+		dueDate
+	}
+
+	return action;
+}
+
+export const deleteReminder = (id) => {
+	const action = {
+		type: DELETE_REMINDER,
+		id
+	}
+
+	return action;
+}
+
+export const clearReminders = () => {
+
+
+	return {
+		type: CLEAR_REMINDERS
+	};
+}
